6924e9290400dc813017ef05a47928e1f0b7be56
Douglas <douglas_san@hotmail.com>
2022-11-18 11:13:47 +0900

    Add files via upload

---
+++ Animal.php
@@ -0,0 +1,19 @@
+<?php
+class Animal {
+	public $atribute1;
+	private $atribute2;
+	protected $atribute3;
+
+	public __construct() {
+		//
+	}
+
+	public getAtribute1() {
+		return $this->atribute1;
+	}
+	public setAtribute1() {
+		$this->atribute1 = $atribute;
+	}
+}
+
+?>
